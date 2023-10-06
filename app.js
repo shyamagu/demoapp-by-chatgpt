119e820c4dba605dae46f0b65f010265ea25b02f
shyamagu <shyamagu@outlook.com>
2023-10-06 13:55:55 +0900

    test2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser'); // 追加
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true })); // 追
 app.use(bodyParser.json()); // 追加
